c83feca870eff104fb7ce20a3945a3f9b4fb3738
vinodkottem <vinodkottem@gmail.com>
2017-10-09 22:39:33 +0530

    template implementation
    next: template caching and handler simplification

--- go/src/webserver/wiki.go
+++ go/src/webserver/wiki.go
@@ -1,61 +1,81 @@
 package main
-  
-  import (
-    "fmt"
+
+import (
+    "html/template"
     "io/ioutil"
     "net/http"
-  )
-  
-  type Page struct {
+)
+
+type Page struct {
     Title string
     Body  []byte
-  }
-  
-  func (p *Page) save() error {
+}
+
+func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
-  }
-  
-  func loadPage(title string) (*Page, error) {
+}
+
+func loadPage(title string) (*Page, error) {
     filename := title + ".txt"
     body, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
     }
     return &Page{Title: title, Body: body}, nil
-  }
-  
-  func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-  }
+}
 
-  func editHandler(w http.ResponseWriter, r *http.Request) {
+//func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+//    t, _ := template.ParseFiles(tmpl + ".html")
+//    t.Execute(w, p)
+//}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, err := template.ParseFiles(tmpl + ".html")
+    if err != nil {
+        http.Error(w,err.Error(),http.StatusInternalServerError)
+        return
+    }
+    err = t.Execute(w, p)
+    if err != nil {
+        http.Error(w, err.Error(),http.StatusInternalServerError)
+    }
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/edit/"):]
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
-    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-        "<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.Body)
-    }
+    renderTemplate(w, "edit", p)
+}
 
-    func saveHandler(w http.ResponseWriter, r *http.Request){
-        title := r.URL.Path[len("/save/"):]
-        p1 := &Page{Title: title, Body: []byte("This is a sample Page.")}
-        p1.save()
-        p2, _ := loadPage(title)
-        fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p2.Title, p2.Body)
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    err := p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
     }
-  
-  func main() {
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
     http.HandleFunc("/view/", viewHandler)
     http.HandleFunc("/edit/", editHandler)
     http.HandleFunc("/save/", saveHandler)
     http.ListenAndServe(":7771", nil)
-  }
+}
